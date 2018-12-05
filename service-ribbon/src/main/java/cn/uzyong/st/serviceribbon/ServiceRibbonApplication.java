fd0885308870047458db6d7b5a9d8ce88c7567d4
uzyong <18566221174@163.com>
2018-12-05 22:59:48 +0800

    [feat]增加工程：spring-ribbon,端口是8781

---
+++ service-ribbon/src/main/java/cn/uzyong/st/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,26 @@
+package cn.uzyong.st.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    @Bean           //注册resttemplate
+    @LoadBalanced
+        //开启负载均衡  一定要开启这个才有效
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
