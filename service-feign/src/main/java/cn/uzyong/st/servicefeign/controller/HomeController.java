602ec89a8de1c97fc6547448d859b3ef391993dd
uzyong <18566221174@163.com>
2018-12-05 23:14:32 +0800

    [feat]增加工程：spring-feign,端口是8782

---
+++ service-feign/src/main/java/cn/uzyong/st/servicefeign/controller/HomeController.java
@@ -0,0 +1,20 @@
+package cn.uzyong.st.servicefeign.controller;
+
+import cn.uzyong.st.servicefeign.service.FeignService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @Autowired
+    FeignService feignService;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name") String name) {
+        return feignService.home(name);
+    }
+
+}
