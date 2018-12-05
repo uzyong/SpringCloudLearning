8500ec33222a5437f30da7a322034e326f829e9f
uzyong <18566221174@163.com>
2018-12-05 16:33:40 +0800

    [feat]新建springcloud工程：server

---
+++ eureka-server-8761/src/main/java/cn/uzyong/st/eurekaserver8761/EurekaServer8761Application.java
@@ -0,0 +1,14 @@
+package cn.uzyong.st.eurekaserver8761;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer8761Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer8761Application.class, args);
+    }
+}
