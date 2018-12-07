213b33181f077fa57f519676d8254ffdc89ee2f0
uzyong <18566221174@163.com>
2018-12-07 11:26:49 +0800

    [feat]configclient添加一个值

--- config-client/src/main/java/cn/uzyong/st/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/cn/uzyong/st/configclient/ConfigClientApplication.java
@@ -3,12 +3,13 @@ package cn.uzyong.st.configclient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
-//@RefreshScope
+@RefreshScope //自动刷新bean
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
